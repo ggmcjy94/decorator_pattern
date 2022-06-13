dbc5b7789a4c3a3fc8dcd2e0a2fed434e989a959
“ggmcjy94” <lhs01071526524@gmail.com>
2022-06-13 13:31:27 +0900

    데코레이터 패턴

---
+++ src/main/java/BaseComponent.java
@@ -0,0 +1,6 @@
+public class BaseComponent implements Component{
+    @Override
+    public String add() {
+        return "에스프레소";
+    }
+}
---
+++ src/main/java/Decorator.java
@@ -0,0 +1,13 @@
+public class Decorator implements Component{
+
+    private Component coffeeComponent;
+
+    public Decorator(Component coffeeComponent) {
+        this.coffeeComponent = coffeeComponent;
+    }
+
+    @Override
+    public String add() {
+        return coffeeComponent.add();
+    }
+}
